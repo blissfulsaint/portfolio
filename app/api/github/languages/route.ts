33509bd05f1a437622114fde46cac1cbc3589f16
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-04-04 23:21:37 +0000

    Fetch private repo info in GitHubLanguages stats

--- app/api/github/languages/route.ts
+++ app/api/github/languages/route.ts
@@ -5,13 +5,19 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 });
 
+const privateOctokit = new Octokit({
+  auth: process.env.PRIVATE_GITHUB_TOKEN,
+});
+
 export async function GET() {
   try {
-    const reposResponse = await octokit.repos.listForAuthenticatedUser({
-      per_page: 100,
-      affiliation: "owner,collaborator,organization_member",
-    });
+    const reposResponse = await octokit.request('GET /user/repos?page=1&per_page=1000', { type: 'owner' });
 
+    const privateRepo = await privateOctokit.request('GET /repos/blissfulsaint/htdocs/languages', {
+      owner: 'blissfulsaint',
+      repo: 'htdocs',
+    })
+    
     const repos = reposResponse.data;
 
     const languageTotals: Record<string, number> = {};
@@ -20,18 +26,26 @@ export async function GET() {
 
     for (const repo of repos) {
       if (repo.fork) continue;
-      console.log("Repo: ", repo.name, "Private?", repo.private)
 
       const langResponse = await octokit.repos.listLanguages({
         owner: repo.owner.login,
         repo: repo.name,
       });
 
+      console.log(langResponse.data);
+
       for (const [language, bytes] of Object.entries(langResponse.data)) {
         // Estimate lines of code (~60 bytes per line)
         const estimatedLines = Math.round((bytes as number) / 60);
         languageTotals[language] = (languageTotals[language] || 0) + estimatedLines;
       }
+    }
+
+    // *** This implementation needs improvement in the future, it will suffice for now ***
+    for (const [language, bytes] of Object.entries(privateRepo.data)) {
+      // Estimate lines of code (~60 bytes per line)
+      const estimatedLines = Math.round((bytes as number) / 60);
+      languageTotals[language] = (languageTotals[language] || 0) + estimatedLines;
     }
 
     return NextResponse.json(languageTotals);
