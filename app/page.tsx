b03ea5d60164bd6768aafe1f220c68074b01793a
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-04-10 22:42:24 +0000

    Update theme, add placeholder --primary-color

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,9 @@ export default function Home() {
       <Hero title="Welcome to BlissHub!" imgSrc="/hiking.jpg" />
       <LayoutBand>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-3">
-          <CardLink title="Example" textClassName="bg-blue-500" imgSrc="/blisskit-ui.png" />
-          <CardLink title="Example" textClassName="bg-blue-500" imgSrc='/vmc.jpeg' />
-          <CardLink title="Example" textClassName="bg-blue-500" imgSrc="/stylestage.png" />
+          <CardLink title="BlissKit-UI" textClassName="bg-primaryColor" imgSrc="/blisskit-ui.png" />
+          <CardLink title="Valley Music Club" textClassName="bg-primaryColor" imgSrc='/vmc.jpeg' />
+          <CardLink title="Style Stage SASS" textClassName="bg-primaryColor" imgSrc="/stylestage.png" />
         </div>
         <GitHubContributionGrid />
         <GitHubLanguagesChart />
