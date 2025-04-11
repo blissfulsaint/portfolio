01facf338e64742940ad43470cc8cf0a6879ebc2
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-04-11 18:53:04 +0000

    Refine styles on home page, add new heading

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import { LayoutBand, CardLink } from "blisskit-ui";
+import { LayoutBand, CardLink, Separator } from "blisskit-ui";
 
 import Hero from "./components/Hero/Hero";
 import GitHubContributionGrid from "./components/GitHubContributionGrid/GitHubContributionGrid";
@@ -15,12 +15,16 @@ export default function Home() {
     <>
       <Hero title="Welcome to BlissHub!" imgSrc="/hiking.jpg" />
       <LayoutBand>
+        <Separator size="xs" />
+        <h3 className="font-semibold mb-6 text-center">Featured Projects</h3>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-3">
           <CardLink title="BlissKit-UI" textClassName="bg-primaryColor" imgSrc="/blisskit-ui.png" />
           <CardLink title="Valley Music Club" textClassName="bg-primaryColor" imgSrc='/vmc.jpeg' />
           <CardLink title="Style Stage SASS" textClassName="bg-primaryColor" imgSrc="/stylestage.png" />
         </div>
+        <Separator size="xxs" />
         <GitHubContributionGrid />
+        <Separator size="xs" />
         <GitHubLanguagesChart />
       </LayoutBand>
     </>
