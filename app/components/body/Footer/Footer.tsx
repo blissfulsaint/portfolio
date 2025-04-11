ce5f1fda72cb0e5c4c3f5cfcff2fa14054a203f1
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-04-11 18:41:39 +0000

    Refine Footer, remove light mode functionality

--- app/components/body/Footer/Footer.tsx
+++ app/components/body/Footer/Footer.tsx
@@ -6,8 +6,28 @@ export default function Footer() {
     return (
         <footer className='mt-24'>
             <hr className={styles.footerborder}></hr>
-            <div className='m-auto w-fit flex flex-wrap gap-2 p-12'>
+            <div className='m-auto w-fit p-12'>
                 <p className='block my-auto'>&copy; 2025 Brandon Lisonbee · Built with Next.js · Hosted on Vercel</p>
+                <div className='flex flex-wrap gap-8 justify-center'>
+                    <Link href='https://github.com/blissfulsaint' aria-label='Brandon Lisonbee on GitHub'>
+                        <Image 
+                            src='/github-white.png'
+                            width={70}
+                            height={70}
+                            alt='GitHub logo'
+                            className='w-14 h-14 my-auto'
+                        />
+                    </Link>
+                    <Link href='https://www.linkedin.com/in/brandon-lisonbee/' aria-label='Brandon Lisonbee on LinkedIn'>
+                        <Image 
+                            src='/linkedin.png'
+                            width={70}
+                            height={70}
+                            alt='LinkedIn logo'
+                            className='w-14 h-14 my-auto'
+                        />
+                    </Link>
+                </div>
             </div>
         </footer>
     )
