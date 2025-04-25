183a47d8399d1329b12d6bd538cdd1b37070dd1d
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-04-25 19:16:42 +0000

    Refine styles for Timeline elements

--- app/components/timeline/Timeline/Timeline.tsx
+++ app/components/timeline/Timeline/Timeline.tsx
@@ -2,6 +2,10 @@ import TimelineItem from "../TimelineItem/TimelineItem"
 
 export default function Timeline() {
     return (
-        <TimelineItem />
+        <>
+            <TimelineItem>Sample item 1</TimelineItem>
+            <TimelineItem>Sample item 2</TimelineItem>
+            <TimelineItem>Sample item 3</TimelineItem>
+        </>
     )
 }
--- app/components/timeline/TimelineItem/TimelineItem.tsx
+++ app/components/timeline/TimelineItem/TimelineItem.tsx
@@ -1,5 +1,24 @@
-export default function TimelineItem() {
+import clsx from "clsx";
+
+interface TimelineItemProps {
+    children?: React.ReactNode;
+    className?: string;
+}
+
+export default function TimelineItem({
+    children,
+    className,
+    ...rest
+}: TimelineItemProps) {
     return (
-        <p>This is the TimelineItem component</p>
+        <div 
+            className={clsx(
+                'border-l-2 border-l-slate-500 px-4 pb-8 relative before:absolute before:size-3 before:rounded-full before:bg-slate-500 before:-left-[7px]',
+                className
+            )}
+            {...rest}
+        >
+            {children}
+        </div>
     )
 }
