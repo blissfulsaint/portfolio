4562f873af3f0770b94ed9bd2babe45f44ddc014
Brandon Lisonbee <blissfulsaint99@gmail.com>
2025-05-01 21:04:22 +0000

    Consume projects.json in FeaturedWorks

--- app/components/FeaturedWorks/FeaturedWorks.tsx
+++ app/components/FeaturedWorks/FeaturedWorks.tsx
@@ -4,11 +4,29 @@ import { useState } from "react";
 
 import Modal from "../Modal/Modal";
 
+import projectsData from '@/app/_data/projects.json';
+
+interface Project {
+    imgSrc: string;
+    title: string;
+    link?: string;
+    github?: string;
+    relatedLinks?: {
+        linkTitle: string;
+        linkHref: string;
+    }[];
+    featured?: boolean;
+}
+
 export default function FeaturedWorks() {
+    const projects = projectsData.filter((project) => project.featured) as Project[];
+
     const [isOpen, setIsOpen] = useState(false);
     const [isVisible, setIsVisible] = useState(false);
+    const [currentProject, setCurrentProject] = useState<Project | null>(null);
 
-    const openModal = () => {
+    const openModal = (index: number) => {
+        setCurrentProject(projects[index]);
         setIsVisible(true);
         setIsOpen(true);
     };
@@ -16,15 +34,18 @@ export default function FeaturedWorks() {
     const closeModal = () => {
         setIsOpen(false);
         setTimeout(() => setIsVisible(false), 250);
+        setCurrentProject(null);
     };
 
     return (
         <>
             <h3 className="font-semibold mb-6 text-center">Featured Projects</h3>
             <CardLinkGrid>
-                <CardLink href="https://github.com/blissfulsaint/blisskit-ui" onClick={openModal} title="BlissKit-UI" textClassName="bg-primaryColor" imgSrc="/blisskit-ui.png" />
-                <CardLink href="https://github.com/blissfulsaint/valleymusicclub" title="Valley Music Club" textClassName="bg-primaryColor" imgSrc='/vmc.jpeg' />
-                <CardLink href="https://github.com/blissfulsaint/stylestage-sass" title="Style Stage SASS" textClassName="bg-primaryColor" imgSrc="/stylestage.png" />
+                {projects && projects.map((project, index) => {
+                    return (
+                        <CardLink onClick={() => openModal(index)} title={project.title} imgSrc={project.imgSrc} textClassName="bg-primaryColor" key={index} />
+                    )
+                })}
             </CardLinkGrid>
 
             <Modal isOpen={isOpen} isVisible={isVisible} onClose={closeModal}>
